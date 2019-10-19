6b521050cf9b3af17a107b7de1694b1fa02a6f98
Nicola Galeotti <nicola.galeotti9@gmail.com>
2019-10-19 13:07:35 +0100

    fine scrittura codice programma

--- Convenienza/Program.cs
+++ Convenienza/Program.cs
@@ -10,6 +10,21 @@ namespace Convenienza
         {
             Console.WriteLine("Inserisci il prezzo totale: ");
             double totale = double.Parse(Console.ReadLine());
+            double n = ScontoNegozio.Negozio(totale);
+            double n12 = ScontoNegozio12.Negozio12(totale);
+            if (n < n12)
+            {
+                Console.WriteLine("E' più conveniente il primo negozio");
+            }
+            else if (n > n12)
+            {
+                Console.WriteLine("E' più conveniente il primo negozio");
+            }
+            else
+            {
+                Console.WriteLine("Sono ugualmente convenienti");
+            }
+            Console.ReadLine();
         }
     }
 }
